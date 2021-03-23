c25d24bb3a628ff829f9e30f22f8adeb3cb38bd6
eggplantzzz <tmfeickert@gmail.com>
2021-03-23 15:39:23 -0400

    Remove Console-related stuff from test

--- test/TestMetaCoin.sol
+++ test/TestMetaCoin.sol
@@ -5,14 +5,8 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/MetaCoin.sol";
 
-// Console.sol can be imported to handle logging in the test environment
-import "truffle/Console.sol";
-
 contract TestMetaCoin {
   function testInitialBalanceUsingDeployedContract() public {
-    // the log method can be used to print string literals
-    Console.log("testing initial balance of the deployed contract");
-
     MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
 
     uint expected = 10000;
@@ -25,9 +19,6 @@ contract TestMetaCoin {
 
     address receiversAddress = address(0x1234567890123456789012345678901234567890);
 
-    // the log method also accepts a label with a variable
-    Console.log("mint 500 coins at ", receiversAddress);
-  
     meta.mintCoins(receiversAddress, 500);
 
     uint expected = 500;
@@ -41,13 +32,11 @@ contract TestMetaCoin {
     address sendersAddress = address(0x0987654321098765432109876543210987654321);
 
     meta.mintCoins(sendersAddress, 500);
-    Console.log("the sender's balance is", meta.getBalance(sendersAddress));
-    Console.log("mint 500 coins at", sendersAddress);
 
     address receiversAddress = address(0x1987654321198765432119876543211987654321);
 
     Console.log("5 ether worth of coins to", receiversAddress);
-  
+
     meta.transferCoins(sendersAddress, receiversAddress, 5);
 
     uint expected = 20;
