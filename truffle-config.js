f59a0411d12b10e663929eff03a1c34bfe23ad89
eggplantzzz <tmfeickert@gmail.com>
2021-03-23 11:06:07 -0400

    Simplify config and change some pragma expressions

--- truffle-config.js
+++ truffle-config.js
@@ -1,40 +1,7 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const mnemonic = "add your mnemonic here";
-const providerOrUrl = "add your provider url here";
-
 module.exports = {
   compilers: {
     solc: {
-      version: "0.7.6",
-      // version: "pragma"
+      version: "pragma"
     }
-  },
-
-  networks: {
-    // to use ethPM v3 you need to have a configuration to connect to Ropsten
-    ropsten: {
-      provider: () => new HDWalletProvider({
-        mnemonic,
-        providerOrUrl
-      }),
-      network_id: "3"
-    }
-  },
-
-  // the following configuration enables the use of ethPM v3
-  ethpm: {
-    ipfsHost: "ipfs.infura.io",
-    ipfsProtocol: "https",
-    ipfsPort: "5001",
-    registry: {
-      address: "0x0bd0200357D26A0bB5d1D1c1Fd56C317B68d15d5",
-      network: "ropsten",
-    },
-    version: "3"
-  },
-
-  // ens must be enabled for ethPM v3
-  ens: {
-    enabled: true
   }
 }
